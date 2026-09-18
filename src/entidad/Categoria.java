1f80a7244cb66068e7b2f38d52d7ce92a86c03bc
SantiB <151892199+SanB00@users.noreply.github.com>
2026-09-18 11:22:19 -0300

    de Lautaro Valentin Bravo - lunes, 14 de septiembre de 2026, 11:27 Hola buenos días!!
    Continúe el tp definiendo los atributos de la clase "Categoría", con sus constructores, getters y setters.
    
    image.png
    
     TP3_GRUPO_16v2.zip

--- src/entidad/Categoria.java
+++ src/entidad/Categoria.java
@@ -1,5 +1,47 @@
 package entidad;
 
 public class Categoria {
-
+		private int IdCategoria;
+		private String Nombre;
+		
+	    // Constructores
+		
+		public Categoria() {
+	
+		}
+		
+		public Categoria(String Nombre) {
+			this.Nombre = Nombre;
+		}
+		
+		public Categoria(int IdCategoria) {
+			this.IdCategoria = IdCategoria;
+		}
+		
+		public Categoria(int IdCategoria,String Nombre) {
+			this.IdCategoria = IdCategoria;
+			this.Nombre = Nombre;
+		}
+		
+	    // Getters y Setters
+		
+		public int getIdCategoria() {
+			return IdCategoria;
+		}
+		public void setIdCategoria(int idCategoria) {
+			IdCategoria = idCategoria;
+		}
+		public String getNombre() {
+			return Nombre;
+		}
+		public void setNombre(String nombre) {
+			Nombre = nombre;
+		}
+		
+	    // ToString
+		
+		@Override
+		public String toString() {
+			return "Categoria [IdCategoria=" + IdCategoria + ", Nombre=" + Nombre + "]";
+		}
 }
