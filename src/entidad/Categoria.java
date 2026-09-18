0cbc72db20307d0292c4965438c9cca311a77b87
SantiB <151892199+SanB00@users.noreply.github.com>
2026-09-18 11:26:40 -0300

    de Macarena Gottig - jueves, 17 de septiembre de 2026, 14:57 Buenas tardes! Realicé cambios en la clase "Categoría" ya que la variable ID es autoincremental, la base de datos se encarga de la misma, por lo tanto no debería poder ser modificada. Y además, comencé con la clase "DaoCategorías", declaré las variables necesarias y desarrollé el método encargado de agregar.
     TP3_GRUPO_16v5.zip

--- src/entidad/Categoria.java
+++ src/entidad/Categoria.java
@@ -1,11 +1,11 @@
 package entidad;
 
 public class Categoria {
+	
 		private int IdCategoria;
 		private String Nombre;
 		
 	    // Constructores
-		
 		public Categoria() {
 	
 		}
@@ -14,23 +14,11 @@ public class Categoria {
 			this.Nombre = Nombre;
 		}
 		
-		public Categoria(int IdCategoria) {
-			this.IdCategoria = IdCategoria;
-		}
-		
-		public Categoria(int IdCategoria,String Nombre) {
-			this.IdCategoria = IdCategoria;
-			this.Nombre = Nombre;
-		}
-		
 	    // Getters y Setters
-		
 		public int getIdCategoria() {
 			return IdCategoria;
 		}
-		public void setIdCategoria(int idCategoria) {
-			IdCategoria = idCategoria;
-		}
+		
 		public String getNombre() {
 			return Nombre;
 		}
@@ -39,9 +27,8 @@ public class Categoria {
 		}
 		
 	    // ToString
-		
 		@Override
 		public String toString() {
-			return "Categoria [IdCategoria=" + IdCategoria + ", Nombre=" + Nombre + "]";
+			return "Categoria N°" + IdCategoria + "| Nombre:" + Nombre;
 		}
 }
